8c45bf71ac86026623eb9eececee1f6e010199b4
AbyOS <105584153+AbyOS@users.noreply.github.com>
2022-09-20 21:32:56 +0300

    Update Download.js

--- src/components/Download.js
+++ src/components/Download.js
@@ -5,13 +5,13 @@ function Download() {
                 Ready, Set, Go!
             </div>
 
-            <a className="button" href="https://github.com/skiftOS/skift/releases">
+            <a className="button" href="https://github.com/AbyOS-Team/aby/releases">
                 Download the Latest Release*
             </a>
         </div>
 
         <div className="text-xs pt-8">
-            * skiftOS is alpha software, use it at your own risk.
+            * AbyOS is alpha software, use it at your own risk.
         </div>
     </div>
 }
