0821c4c11f8959ff5ce578052138db803d63db87
AbyOS <105584153+AbyOS@users.noreply.github.com>
2022-09-20 21:55:43 +0300

    Update Contribute.js

--- src/components/Contribute.js
+++ src/components/Contribute.js
@@ -1,6 +1,6 @@
 function Contribute() {
     return <div className="flex flex-col items-center bg-blue-600">
-            <a href="https://github.com/skiftOS/skift" className="flex items-center gap-4 p-4 md:p-12">
+            <a href="https://github.com/AbyOS-Team/aby" className="flex items-center gap-4 p-4 md:p-12">
                 <img src="github.svg" className="h-16" alt=""/>
                 <div className="flex flex-col">
                     <div className="text-2xl font-extrabold mb-2">
